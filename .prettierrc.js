15bfba19ae150fc20692f2eff2cb14a36dc126ce
Le Hoang Tu <mrahn1234@gmail>
2025-12-14 21:07:00 +0900

    setup

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// .prettierrc.js (Đã được chuyển đổi)
+
+// Không cần import Config Type nữa
+// const config: Config = { ...
+
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: 'always',
+
+  // Plugin Tailwind CSS
+  plugins: ['prettier-plugin-tailwindcss'],
+}
